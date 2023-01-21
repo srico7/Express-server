e727bea5e154299b44a0499d214bce3439fe2168
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-01-21 15:02:58 +0530

    Using nodemon

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.get('/about', (req, res) => res.send('Hello, My name is Chathura Munasinghe!'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-    });
+    }); 

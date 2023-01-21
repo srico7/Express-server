3d6dfb545f8d5777a842407587ca57e702fffa12
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-01-21 15:20:07 +0530

    npm body-parser

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.post('/', (req, res) => {
+    res.send('Thanks for posting that!');
+    });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);

4937d7a84f8635c04bf5bc1319711327102b40a6
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-01-21 16:14:15 +0530

    server side BMI calculator

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = 3000;
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 app.post('/', (req, res) => {
-    res.send('Thanks for posting that!');
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send('The final result of the calculation is ' + result);
+    });
+    
+    app.get('/bmicalculator', (req, res) => res.sendFile(__dirname + '/bmiCalculator.html'));
+
+    app.post('/bmiCalculator', (req, res) => {
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send('Your BMI is ' + bmi);
     });
 
 app.listen(port, () => {

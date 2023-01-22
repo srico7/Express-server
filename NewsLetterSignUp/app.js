64bf73e4c9659bb9d288ba29a7a4c0e685e5c333
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-01-22 12:35:44 +0530

    Mailchimp API Connected

--- NewsLetterSignUp/app.js
+++ NewsLetterSignUp/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
 
@@ -11,36 +12,39 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
-
-    // var data = {
-    //     members: [
-    //         {
-    //             email_address: email,
-    //             status: "subscribed",
-    //             merge_fields: {
-    //                 FNAME: firstName,
-    //                 LNAME: lastName
-    //             }
-    //         }
-    //     ]
-    // };
-
-    // var jsonData = JSON.stringify(data);
-
-    // var options = {
-    //     url: "https://usX.api.mailchimp.com/3.0/lists/xxxxxxxxxx",
-    //     method: "POST",
-    //     headers: {
-
-    //     },
+mailchimp.setConfig({
+    apiKey : "0034a5608c11efe04f396619df1cdf64-us21",
+    server : "us21" 
 });
+app.post("/", function(req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const listId = "bffbcae121";
+
+    const subscribingUser = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email
+    };
+
+    async function run() {
+        const response = await mailchimp.lists.addListMember(listId, { 
+            email_address: subscribingUser.email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: subscribingUser.firstName,
+                LNAME: subscribingUser.lastName
+            } 
+        });
+
+        res.sendFile(__dirname + "/success.html");
+    }
+
+    run().catch(e => res.sendFile(__dirname + "/failure.html"));
+});
+
 app.listen(3000, function() {
     console.log("Server is running on port 3000.");
     });
-
-    64aa3f6805dd3ea7ff9c775be9e45a7d-us21
-

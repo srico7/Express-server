be7caaa6c14b3d70e7bd1d13460c7d5d4d62c6b9
Chathura Munasinghe <mactmunasinghe@students.nsbm.ac.lk>
2023-01-22 17:00:50 +0530

    Procfile added

--- NewsLetterSignUp/app.js
+++ NewsLetterSignUp/app.js
@@ -49,6 +49,6 @@ app.post("/failure.html", function(req, res) {
     res.redirect("/");
   });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port 3000.");
     });
